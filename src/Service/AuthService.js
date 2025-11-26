4a0d0de2e8e89cd72fa910a9b038660f56bda8df
vijay-msrit <vijay-msrit@github.com>
2025-11-26 20:39:02 +0530

    Refactor: merged all service files into single api.js and updated API calls

--- src/Service/AuthService.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const login = async (data) => {
-    return await axios.post("https://billing-software-backend-1-5hah.onrender.com/api/v1.0/login", data);
-}
--- src/Service/Dashboard.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const fetchDashboardData = async () => {
-    return await axios.get(" https://billing-software-backend-1-5hah.onrender.com/api/v1.0/dashboard", {headers: {'Authorization': `Bearer ${localStorage.getItem("token")}`}});
-}
-
